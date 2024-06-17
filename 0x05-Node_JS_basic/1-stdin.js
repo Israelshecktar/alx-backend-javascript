5ce7cb13ae901ec1619e833b4ba0342032f1654b
Israelshecktar <iamshecktar1996@gmail.com>
2024-06-17 19:57:23 +0000

    fixed lint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 // Scripts to write to stdinput
 
-process.stdout.write(`Welcome to Holberton School, what is your name?\n`);
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
