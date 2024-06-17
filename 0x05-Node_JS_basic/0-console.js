629638a1ea52de755b0c335e7aeba7eae002c2db
Israelshecktar <iamshecktar1996@gmail.com>
2024-06-17 17:41:42 +0000

    lint the code

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,6 +1,6 @@
 // function to display message to stdout
 function displayMessage(message) {
-    console.log(message);
+  console.log(message);
 }
 
-module.exports = displayMessage
+module.exports = displayMessage;
