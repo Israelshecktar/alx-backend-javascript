c2a78f419799f0ecd5709cd4a9c73210ca74b44f
Israelshecktar <iamshecktar1996@gmail.com>
2024-06-18 15:48:03 +0000

    express js

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+// Creating a simple server with express js
+const express = require('express');
+
+const app = express();
+
+const PORT = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+module.exports = app;
