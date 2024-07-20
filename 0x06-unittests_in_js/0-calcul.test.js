512e86a3c7565f1b95aaf18c3038f67b80e285d6
Israelshecktar <iamshecktar1996@gmail.com>
2024-07-20 10:18:47 +0000

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -17,4 +17,23 @@ describe('calculateNumber', function() {
   it('should return 6 when inputs are 1.5 and 3.7', function() {
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
   });
+
+  // Test suite for second number rounded
+  describe('when the second number is rounded', function() {
+    it('should return 5 when inputs are 1 and 4.4', function() {
+      assert.strictEqual(calculateNumber(1, 4.4), 5);
+    });
+
+    it('should return 6 when inputs are 1 and 4.5', function() {
+      assert.strictEqual(calculateNumber(1, 4.5), 6);
+    });
+
+    it('should return 6 when inputs are 1 and 4.6', function() {
+      assert.strictEqual(calculateNumber(1, 4.6), 6);
+    });
+
+    it('should return 7 when inputs are 1 and 5.5', function() {
+      assert.strictEqual(calculateNumber(1, 5.5), 7);
+    });
+  });
 });
